6164e7c30e7386f6be93371081704ef7538c4bb4
ongamex <ongamex@windowslive.com>
2025-01-23 02:51:12 +0200

    - the vertices are now passed via a shader storage buffer

--- app/main.cpp
+++ app/main.cpp
@@ -29,9 +29,10 @@ std::vector<char> readFile(const char* const filename)
 
 struct AppState {
 	SDL_Window* wnd = nullptr;
-	SDL_GPUDevice* wndDevice = nullptr;
+	SDL_GPUDevice* gpuDevice = nullptr;
 	SDL_GPUShader* vertexShader = nullptr;
 	SDL_GPUShader* fragShader = nullptr;
+	SDL_GPUBuffer* vertexBuffer = nullptr;
 	SDL_GPUGraphicsPipeline* trianglePipeline = nullptr;
 };
 
@@ -47,9 +48,9 @@ SDL_AppResult SDL_AppInit(void** appstateRaw, int argc, char* argv[])
 
 
 	appState.wnd = SDL_CreateWindow("App test SDL3", 1024, 768, 0);
-	appState.wndDevice = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV, true, NULL);
+	appState.gpuDevice = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV, true, NULL);
 
-	bool succ = SDL_ClaimWindowForGPUDevice(appState.wndDevice, appState.wnd);
+	bool succ = SDL_ClaimWindowForGPUDevice(appState.gpuDevice, appState.wnd);
 
 	// Shaders:
 	{
@@ -63,11 +64,11 @@ SDL_AppResult SDL_AppInit(void** appstateRaw, int argc, char* argv[])
 		ci.stage = SDL_GPU_SHADERSTAGE_VERTEX;
 		ci.num_samplers = 0;
 		ci.num_storage_textures = 0;
-		ci.num_storage_buffers = 0;
+		ci.num_storage_buffers = 1;
 		ci.num_uniform_buffers = 0;
 		ci.props = 0;
 
-		appState.vertexShader = SDL_CreateGPUShader(appState.wndDevice, &ci);
+		appState.vertexShader = SDL_CreateGPUShader(appState.gpuDevice, &ci);
 	}
 
 	{
@@ -85,13 +86,15 @@ SDL_AppResult SDL_AppInit(void** appstateRaw, int argc, char* argv[])
 		ci.num_uniform_buffers = 0;
 		ci.props = 0;
 
-		appState.fragShader = SDL_CreateGPUShader(appState.wndDevice, &ci);
+		appState.fragShader = SDL_CreateGPUShader(appState.gpuDevice, &ci);
 	}
+
+
 
 	{
 		SDL_GPUColorTargetDescription colorTargetDesc;
 		memset(&colorTargetDesc, 0, sizeof(colorTargetDesc)); // For the blend state which is not initialized.
-		colorTargetDesc.format = SDL_GetGPUSwapchainTextureFormat(appState.wndDevice, appState.wnd);
+		colorTargetDesc.format = SDL_GetGPUSwapchainTextureFormat(appState.gpuDevice, appState.wnd);
 
 		SDL_GPUColorTargetDescription allColorTargetDesc[] = {
 			colorTargetDesc
@@ -105,7 +108,74 @@ SDL_AppResult SDL_AppInit(void** appstateRaw, int argc, char* argv[])
 		pipelineCreateInfo.fragment_shader = appState.fragShader;
 		pipelineCreateInfo.rasterizer_state.fill_mode = SDL_GPU_FILLMODE_FILL;
 
-		appState.trianglePipeline = SDL_CreateGPUGraphicsPipeline(appState.wndDevice, &pipelineCreateInfo);
+		appState.trianglePipeline = SDL_CreateGPUGraphicsPipeline(appState.gpuDevice, &pipelineCreateInfo);
+	}
+
+	
+
+	// Create the upload to GPU transfer buffer:
+	SDL_GPUTransferBuffer* h2dBuffer = NULL; // host-to-device buffer
+	{
+		SDL_GPUTransferBufferCreateInfo tbi;
+		memset(&tbi, 0, sizeof(tbi));
+		tbi.size = 1024 * 1024 * 1; // 1MB
+		tbi.usage = SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD;
+
+		h2dBuffer = SDL_CreateGPUTransferBuffer(appState.gpuDevice, &tbi);
+	}
+
+	/// Create vertex buffer:
+	{
+		// Buffer initial data
+		struct Vertex {
+			Vertex() = default;
+			Vertex(float x, float y, float z) : x(x), y(y), z(z), padding(0) {}
+
+			float x, y, z;
+			float padding;
+		};
+
+		Vertex vertices[] = {
+			Vertex(0.0, -0.5, 0.f),
+			Vertex(0.5, 0.5, 0.f),
+			Vertex(-0.5, 0.5, 0.f)
+		};
+
+		// copy the vertices to the transfer buffer:
+		void* h2dBufferMem = SDL_MapGPUTransferBuffer(appState.gpuDevice, h2dBuffer, false);
+		memcpy(h2dBufferMem, vertices, sizeof(vertices));
+		SDL_UnmapGPUTransferBuffer(appState.gpuDevice, h2dBuffer);
+		h2dBufferMem = nullptr;
+
+		// Create an empty vertex buffer:
+		SDL_GPUBufferCreateInfo bci = { 0 };
+		bci.usage = SDL_GPU_BUFFERUSAGE_GRAPHICS_STORAGE_READ;
+		bci.size = sizeof(Vertex) * 3;
+
+		appState.vertexBuffer = SDL_CreateGPUBuffer(appState.gpuDevice, &bci);
+		SDL_SetGPUBufferName(appState.gpuDevice, appState.vertexBuffer, "The Vertex Buffer");
+
+		// Perform the copy form the tranfer buffer to the actual vertex buffer:
+		SDL_GPUCommandBuffer* cmdbuf = SDL_AcquireGPUCommandBuffer(appState.gpuDevice);
+		SDL_GPUCopyPass* copyPass = SDL_BeginGPUCopyPass(cmdbuf);
+
+
+		SDL_GPUTransferBufferLocation source;
+		source.offset = 0;
+		source.transfer_buffer = h2dBuffer;
+
+		SDL_GPUBufferRegion dest;
+		dest.buffer = appState.vertexBuffer;
+		dest.offset = 0;
+		dest.size = sizeof(vertices);
+
+		SDL_UploadToGPUBuffer(copyPass, &source, &dest, false);
+
+		// End the pass:
+		SDL_EndGPUCopyPass(copyPass);
+
+		// Execute the pass:
+		SDL_SubmitGPUCommandBuffer(cmdbuf);
 	}
 
 	if (!succ) {
@@ -128,7 +198,7 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 SDL_AppResult SDL_AppIterate(void* appstateRaw)
 {
 	AppState& appState = *(AppState*)(appstateRaw);
-	SDL_GPUCommandBuffer* cmdbuf = SDL_AcquireGPUCommandBuffer(appState.wndDevice);
+	SDL_GPUCommandBuffer* cmdbuf = SDL_AcquireGPUCommandBuffer(appState.gpuDevice);
 
 	SDL_GPUTexture* swapchainTexture;
 	if (!SDL_WaitAndAcquireGPUSwapchainTexture(cmdbuf, appState.wnd, &swapchainTexture, NULL, NULL)) {
@@ -142,10 +212,13 @@ SDL_AppResult SDL_AppIterate(void* appstateRaw)
 	colorTargetInfo.store_op = SDL_GPU_STOREOP_STORE;
 
 	SDL_GPURenderPass* renderPass = SDL_BeginGPURenderPass(cmdbuf, &colorTargetInfo, 1, NULL);
-	SDL_GPUViewport vp = {0,0, 1024, 768, 0.f, 1.f};
-	
+	SDL_GPUViewport vp = { 0,0, 1024, 768, 0.f, 1.f };
+
 	SDL_SetGPUViewport(renderPass, &vp);
 	SDL_BindGPUGraphicsPipeline(renderPass, appState.trianglePipeline);
+
+	SDL_GPUBuffer* buffers[] = { appState.vertexBuffer };
+	SDL_BindGPUVertexStorageBuffers(renderPass, 0, buffers, 1);
 
 	SDL_DrawGPUPrimitives(renderPass, 3, 1, 0, 0);
 	SDL_EndGPURenderPass(renderPass);
--- app/triangle.vert
+++ app/triangle.vert
@@ -14,7 +14,19 @@ vec3 colors[3] = vec3[](
     vec3(0.0, 0.0, 1.0)
 );
 
+struct Vertex {
+    float x;
+    float y;
+    float z;
+    float padding;
+};
+
+layout(std140, set = 0, binding = 0) readonly buffer Vertices {
+	Vertex positions[];
+} vertices;
+
 void main() {
-    gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
+    vec2 pt = vec2(vertices.positions[gl_VertexIndex].x, vertices.positions[gl_VertexIndex].y);
+    gl_Position = vec4(pt, 0.0, 1.0);
     fragColor = colors[gl_VertexIndex];
 }
